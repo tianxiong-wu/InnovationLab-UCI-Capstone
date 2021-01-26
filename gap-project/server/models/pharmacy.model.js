6623a6b999671d49e61096daec4e891ff053f49c
Tianxiong Wu <38987912+tianxiongwu@users.noreply.github.com>
2021-01-25 20:08:51 -0800

    rest models added

--- gap-project/server/models/pharmacy.model.js
+++ gap-project/server/models/pharmacy.model.js
@@ -1,37 +1,29 @@
 const mongoose = require('mongoose');
-const { schema } = require('./tutorial.model');
 const Schema = mongoose.Schema;
 
 const pharmacySchema = new Schema({
     name: {
         type: String,
-        required: true
     },
     streetName: {
         type: String,
-        required: true
     },
     city: {
         type: String,
-        required: true
     },
     state: {
         type: String,
-        required: true
     },
     zipCode: {
         type: String,
-        required: true
     },
     phone: {
         type: String,
-        required: true
     },
     email: {
         type: String,
-        required: true
     }
-})
+});
 
 const Pharmacy = mongoose.model('Pharmacy', pharmacySchema);
 module.exports = Pharmacy;
---
+++ gap-project/server/routes/admin.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+let Admin = require('../models/admin.model');
+
+router.route('/register').post((req, res) =>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const pharmacists = req.body.pharmacists;
+    const pharmacy = req.body.pharmacy;
+
+    const newAdmin = new Admin({
+        firstName,
+        lastName,
+        pharmacists,
+        pharmacy
+    });
+
+    newAdmin.save()
+    .then(() => res.json(newAdmin))
+    .catch(err => res.status(400).json('Error: ' + err))
+    .catch(err => res.status(500).json('Error: ' + err));
+})
+
+module.exports = router;
