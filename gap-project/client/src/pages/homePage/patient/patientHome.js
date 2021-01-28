145427168a0114d3486484db7f3dc5459901fdee
[Abdul] <abdul.osman997@gmail.com>
2021-01-27 22:54:12 -0800

    responsive infusion thumbnail

--- gap-project/client/src/pages/homePage/patient/patientHome.js
+++ gap-project/client/src/pages/homePage/patient/patientHome.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Grid } from "@material-ui/core"
 import { Typography } from "@material-ui/core"
 import '../patient/patientHome.css'
@@ -11,8 +11,8 @@ export default function PatientHome(props){
 
     return(
         <Grid container>
-            <Grid item xs={1} sm={1} md={2} lg={2} xl={2}></Grid>
-            <Grid item xs={1} sm={10} md={8} lg={8} xl={8}>
+            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+            <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
                 <div className="summaryContainer">
                     <Typography variant="h4" className="nextInfusionSummary">Hi {propsName}, your next infusion is on</Typography>
                     <Typography variant="h4" className="nextInfusionSummary">{nextInfusion}</Typography>
@@ -21,7 +21,7 @@ export default function PatientHome(props){
                     <div className="leftWidgets">
                         <div className="infusionWidget">
                             <div className="infusionVideoContainer">
-                                <img src="https://picsum.photos/640/280"></img>
+                                <img src="https://picsum.photos/600/400" className="infusionThumbnail"></img>
                             </div>
                             <div className="infusionLabel">
                                 <Typography variant="h4">Next Infusion: {nextInfusionName}</Typography>
@@ -49,7 +49,7 @@ export default function PatientHome(props){
                     </div>
                 </div>
             </Grid>
-            <Grid item xs={1} sm={1} md={2} lg={2} xl={2}></Grid>
+            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
         </Grid>
     )
 }
