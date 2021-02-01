6ca828fc19d80ff74c15c36556a28538478f9abb
Cheesepuffed <chenalicious10@yahoo.com>
2021-02-01 10:42:54 -0800

    Notification updated
    Still need to add edit button logo
    Still may need some readjusting

--- gap-project/client/src/pages/settings/settings.js
+++ gap-project/client/src/pages/settings/settings.js
@@ -4,6 +4,17 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import '../settings/settings.css'
 
+function switchNotification () {
+	console.log('hello');
+	document.getElementById('securityContainer').style.display = 'none';
+	document.getElementById('notificationsContainer').style.display = 'flex';
+}
+
+function switchSecurity () {
+	console.log('hello');
+	document.getElementById('securityContainer').style.display = 'flex';
+	document.getElementById('notificationsContainer').style.display = 'none';
+}
 
 export default function settings() {
 	return (
@@ -12,12 +23,12 @@ export default function settings() {
         Settings
       </div>
 
-      <div className = 'securityContainer'>
-        <div className = 'securityTitleBar'>
-          <div className = 'securityTitle'>
+      <div id = 'securityContainer'>
+        <div className = 'titleBar'>
+          <div className = 'securityTitle'onClick = {switchSecurity}>
             Security
           </div>
-          <div className = 'notificationTitle'>
+          <div className = 'notificationTitle' onClick = {switchNotification} >
             Notification
           </div>
         </div>
@@ -41,6 +52,93 @@ export default function settings() {
         </div>
       </div>
 
+			<div id = 'notificationsContainer'>
+						<div className = 'titleBar'>
+		          <div className = 'securityTitle'onClick = {switchSecurity}>
+		            Security
+		          </div>
+		          <div className = 'notificationTitle' onClick = {switchNotification} >
+		            Notification
+		          </div>
+		        </div>
+
+						<div className = 'folderBorder'>
+							<div className = 'whiteBorder'>
+			          <div className = 'notificationSection'>
+			              <div className = 'insideTitle'>
+			                Phone Number
+			              </div>
+			              <div className = 'notifyMe'>
+			                Notify Me?
+			              </div>
+										<div className = 'yesContainer'>
+											<div className = 'yes'>
+											Yes
+											</div>
+											<div className = 'yesBox'> </div>
+										</div>
+										<div className = 'noContainer'>
+											<div className = 'no'>
+											No
+											</div>
+			              	<div className = 'noBox'> </div>
+										</div>
+										<div className = 'infoContainer'>
+				              <div className = 'infoBox'>
+				                Call me at:
+				              </div>
+											<div className = 'phone_email_Container'>
+					              <div className = 'phone'>
+					                (408)123-4567
+					              </div>
+					              <div className = 'edit'>
+					                edit
+					              </div>
+											</div>
+										</div>
+			          </div>
+								<div className = 'notificationSection'>
+			              <div className = 'insideTitle'>
+			                Phone Number
+			              </div>
+			              <div className = 'notifyMe'>
+			                Notify Me?
+			              </div>
+										<div className = 'yesContainer'>
+											<div className = 'yes'>
+											Yes
+											</div>
+											<div className = 'yesBox'> </div>
+										</div>
+										<div className = 'noContainer'>
+											<div className = 'no'>
+											No
+											</div>
+			              	<div className = 'noBox'> </div>
+										</div>
+										<div className = 'infoContainer'>
+				              <div className = 'infoBox'>
+				                Email me at:
+				              </div>
+											<div className = 'phone_email_Container'>
+					              <div className = 'email'>
+					                innovationlab@email.com
+					              </div>
+					              <div className = 'edit'>
+					                edit
+					              </div>
+											</div>
+										</div>
+			          </div>
+
+								<div className = 'break'></div>
+
+								<div className = 'saveButton'>
+			            Save
+			          </div>
+							</div>
+						</div>
+      </div>
     </div>
 	);
 }
