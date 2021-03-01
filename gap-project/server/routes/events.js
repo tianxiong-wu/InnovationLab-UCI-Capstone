459b27fcc8b52c542225a7e16e2a161f9f472500
Tianxiong Wu <38987912+tianxiongwu@users.noreply.github.com>
2021-03-01 11:49:26 -0800

    fix

--- gap-project/server/routes/events.js
+++ gap-project/server/routes/events.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 let Event = require('../models/event.model');
 
-router.route('/all').get((res, req) =>{
+router.route('/all').get((req, res) =>{
     Event.find()
         .then(events => res.json(events))
         .catch(err => res.status(500).json('Error: ' + err));
 
 })
 
-router.route('/add').post((res, req) => {
+router.route('/add').post((req, res) => {
     const title = req.body.title;
     const start = Date.parse(req.body.start);
     const end = Date.parse(req.body.end);
@@ -17,17 +17,20 @@ router.route('/add').post((res, req) => {
     const description = req.body.description;
 
     const newEvent = new Event ({
-        title, start, end, notifyAt, description
+        title, 
+        start, 
+        end, 
+        notifyAt, 
+        description
     });
 
     newEvent.save()
         .then(() => res.json(newEvent))
-        .catch(err => res.status(400).json('Error: ' + err))
-        .catch(err => res.status(500).json('Error: ' + err));
+            .catch(err => res.status(400).json('Error: ' + err))
+            .catch(err => res.status(500).json('Error: ' + err));
 
 })
-
-router.route('/delete/:id').post((res, req) => {
+router.route('/delete/:id').post((req, res) => {
     Event.findByIdAndDelete(req.params.id)
         .then(() => res.json('deleted'))
         .catch(err => res.status(400).json('Error: ' + err))
--- gap-project/server/routes/notification.js
+++ gap-project/server/routes/notification.js
@@ -3,14 +3,14 @@ const router = require('express').Router();
 let Notification = require('../models/notification.model');
 
 
-router.route('/all').get((res, req) =>{
+router.route('/all').get((req, res) =>{
     Notification.find()
         .then(notifications => res.json(notifications))
         .catch(err => res.status(500).json('Error: ' + err));
 
 })
 
-router.route('/add').post((res, req) => {
+router.route('/add').post((req, res) => {
     const title = req.body.title;
     const notifyAt = Date.parse(req.body.notifyAt);
     const description = req.body.description;
@@ -26,7 +26,7 @@ router.route('/add').post((res, req) => {
 
 })
 
-router.route('/delete/:id').post((res, req) => {
+router.route('/delete/:id').post((req, res) => {
     Notification.findByIdAndDelete(req.params.id)
         .then(() => res.json('deleted'))
         .catch(err => res.status(400).json('Error: ' + err))
